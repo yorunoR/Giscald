33e67f51ad4ea1a6c5fa2c37e8936a86a9299e59
Yoshioka <sss.yoshioka@gmail.com>
2024-02-05 16:55:31 +0000

    🎸 feat: basic structure of pages

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,51 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// import i18n from './services/i18n'
+
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Avatar from 'primevue/avatar'
+import Button from 'primevue/button'
+import Chip from 'primevue/chip'
+import Dialog from 'primevue/dialog'
+import Dropdown from 'primevue/dropdown'
+import InputNumber from 'primevue/inputnumber'
+import InputText from 'primevue/inputtext'
+import Paginator from 'primevue/paginator'
+import Password from 'primevue/password'
+import ProgressSpinner from 'primevue/progressspinner'
+import RadioButton from 'primevue/radiobutton'
+import Sidebar from 'primevue/sidebar'
+import Textarea from 'primevue/textarea'
+import Toast from 'primevue/toast'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app
+  .use(createPinia())
+  // .use(i18n)
+  .use(router)
+  .use(PrimeVue)
+  .use(ToastService)
+  .component('Avatar', Avatar)
+  .component('Button', Button)
+  .component('Chip', Chip)
+  .component('Dialog', Dialog)
+  .component('Dropdown', Dropdown)
+  .component('InputNumber', InputNumber)
+  .component('InputText', InputText)
+  .component('Paginator', Paginator)
+  .component('Password', Password)
+  .component('ProgressSpinner', ProgressSpinner)
+  .component('RadioButton', RadioButton)
+  .component('Sidebar', Sidebar)
+  .component('Textarea', Textarea)
+  .component('Toast', Toast)
+  .mount('#app')

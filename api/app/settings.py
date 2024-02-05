243f61c367696d456070cab5f6118c5ade50f620
Yoshioka <sss.yoshioka@gmail.com>
2024-02-05 15:04:18 +0000

    🎸 feat: install django-cors-headers and set CORS
    poetry add django-cors-headers

--- api/app/settings.py
+++ api/app/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "django_bootstrap5",
+    "corsheaders",
     "libs.apps.LibsConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -134,3 +136,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "libs.AdminUser"
+
+CORS_ALLOW_ALL_ORIGINS = True

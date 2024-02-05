d60f3cb5ca4c092fd6579081a3f5d3eecee15ef2
Yoshioka <sss.yoshioka@gmail.com>
2024-02-05 16:32:19 +0000

    🎸 feat: set lint check

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -4,12 +4,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+    ]
   }
 }

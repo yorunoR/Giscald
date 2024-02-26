7a62c605d0e0086876e528e595bc6fa1b96c4cda
Yoshioka <sss.yoshioka@gmail.com>
2024-02-26 13:15:24 +0000

    🎸 feat: add signin mutation

--- api/app/graphql/schema.py
+++ api/app/graphql/schema.py
@@ -1,6 +1,7 @@
 import strawberry
 from strawberry_django.optimizer import DjangoOptimizerExtension
 
+from app.graphql.resolvers.mutation.signin import resolve as signin
 from app.graphql.resolvers.query.ping import resolve as ping
 from app.graphql.resolvers.query.users import resolve as users
 from app.graphql.types.resources.user_type import UserType
@@ -12,8 +13,14 @@ class Query:
     users: list[UserType] = strawberry.field(resolver=users)
 
 
+@strawberry.type
+class Mutation:
+    signin: UserType = strawberry.field(resolver=signin)
+
+
 schema = strawberry.Schema(
     query=Query,
+    mutation=Mutation,
     extensions=[
         DjangoOptimizerExtension,  # not required, but highly recommended
     ],

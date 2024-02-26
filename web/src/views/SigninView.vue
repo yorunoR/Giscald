2ad02c91f40ba9a785d87b7d33ab3baadbbbb4cb
Yoshioka <sss.yoshioka@gmail.com>
2024-02-26 13:07:18 +0000

    🎸 feat: add signin page

---
+++ web/src/views/SigninView.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="h-full">
+    <h1 style="margin: 100px">Giscald</h1>
+    <Button class="top-50" @click="signIn">Google Sign in</Button>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useMutation } from '@urql/vue'
+import { useToast } from 'primevue/usetoast'
+import router from '@/router'
+import firebase from '@/services/firebase'
+import { graphql } from '@/gql'
+import Signin from '@/doc/mutation/Signin'
+
+const toast = useToast()
+
+const { executeMutation: signinUser } = useMutation(graphql(Signin))
+
+const signIn = async () => {
+  await firebase.signinWithGoogle()
+  const result = await signinUser({})
+  if (result.error) {
+    toast.add({
+      severity: 'error',
+      summary: 'Sign in',
+      detail: result.error.message
+    })
+  } else {
+    // router.push({ name: 'users' })
+    console.log('signin')
+  }
+}
+</script>

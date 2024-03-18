cfea7a12bffb7357dd7bd5d7fe6293da37bc75e6
Yoshioka <sss.yoshioka@gmail.com>
2024-03-19 00:24:08 +0900

    🎸 feat: add async chat request

---
+++ api/libs/services/gen_answer.py
@@ -0,0 +1,19 @@
+import litellm
+
+# litellm.set_verbose = True
+
+
+async def chat(messages, model, host, api_key, temperature=0, max_tokens=512, top_p=None, stop=None, frequency_penalty=2):
+    response = await litellm.acompletion(
+        messages=messages,
+        model=model,
+        api_base=host,
+        api_key=api_key,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        stop=stop,
+        frequency_penalty=frequency_penalty,
+    )
+
+    return response.choices[0].message.content

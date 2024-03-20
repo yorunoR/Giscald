a843262a89d89af4116ac347b56d8b5535219162
Yoshioka <sss.yoshioka@gmail.com>
2024-03-20 14:07:04 +0000

    🎸 feat: add EvaluationTask and Rate models

--- api/libs/models/__init__.py
+++ api/libs/models/__init__.py
@@ -2,3 +2,5 @@ from .admin_user import AdminUser
 from .user import User
 from .generation_task import GenerationTask
 from .answer import Answer
+from .evaluation_task import EvaluationTask
+from .rate import Rate
---
+++ api/libs/models/evaluation_task.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from libs.models import GenerationTask, User
+
+from .base import BaseModel
+
+
+class Status(models.IntegerChoices):
+    CREATED = 0, "Created"
+    STARTED = 10, "Started"
+    COMPLETED = 20, "Completed"
+    FAILED = 30, "Failed"
+
+
+class EvaluationTask(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="evaluation_tasks")
+    generation_task = models.ForeignKey(GenerationTask, on_delete=models.CASCADE, related_name="evaluation_tasks")
+    name = models.CharField(max_length=512)
+    points = models.JSONField()
+    status = models.IntegerField(choices=Status.choices, default=Status.CREATED)
+
+    class Meta:
+        db_table = "evaluation_tasks"
+        verbose_name = "評価タスク"
+        verbose_name_plural = "評価タスク"
+        constraints = [
+            models.UniqueConstraint(fields=["generation_task", "name"], name="evaluation_task_unique"),
+        ]

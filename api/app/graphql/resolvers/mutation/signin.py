48df3ca19ad9caa454ac85a933b93f66d45b5d0e
Yoshioka <sss.yoshioka@gmail.com>
2024-03-23 19:47:40 +0000

    🐛 fix: singin resolver with asynchronous support

--- api/app/graphql/resolvers/mutation/signin.py
+++ api/app/graphql/resolvers/mutation/signin.py
@@ -3,12 +3,12 @@ from strawberry.types import Info
 from libs.models import User
 
 
-def resolve(info: Info):
+async def resolve(info: Info):
     try:
         if info.context.user:
             user = info.context.user
         if info.context.uid:
-            user = User.objects.create(uid=info.context.uid, email=info.context.email, activated=True)
+            user = await User.objects.acreate(uid=info.context.uid, email=info.context.email, activated=True)
     except Exception as e:
         raise Exception("Invalid user.") from e
 

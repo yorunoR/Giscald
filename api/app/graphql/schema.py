82263d374a11c5c823926818a438659e22bbcad8
Yoshioka <sss.yoshioka@gmail.com>
2024-03-24 04:45:04 +0000

    🎸 feat: add update evaluation_task mutation and evaluation_tasks query

--- api/app/graphql/schema.py
+++ api/app/graphql/schema.py
@@ -4,6 +4,7 @@ from strawberry_django.optimizer import DjangoOptimizerExtension
 from app.graphql.resolvers.mutation.create_evaluation_task import resolve as create_evaluation_task
 from app.graphql.resolvers.mutation.create_generation_task import resolve as create_generation_task
 from app.graphql.resolvers.mutation.signin import resolve as signin
+from app.graphql.resolvers.mutation.update_evaluation_task import resolve as update_evaluation_task
 from app.graphql.resolvers.query.current_user import resolve as current_user
 from app.graphql.resolvers.query.ping import resolve as ping
 from app.graphql.types.resources.evaluation_task_type import EvaluationTaskType
@@ -22,6 +23,7 @@ class Mutation:
     signin: UserType = strawberry.field(resolver=signin)
     createGenerationTask: GenerationTaskType = strawberry.field(resolver=create_generation_task)
     createEvaluationTask: EvaluationTaskType = strawberry.field(resolver=create_evaluation_task)
+    updateEvaluationTask: EvaluationTaskType = strawberry.field(resolver=update_evaluation_task)
 
 
 schema = strawberry.Schema(

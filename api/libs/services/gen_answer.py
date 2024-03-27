c50122c3292f8dd59c9f8ed5846046c9062a1f64
Yoshioka <sss.yoshioka@gmail.com>
2024-03-27 19:26:31 +0900

    🎸 feat: improved max_tokens control

--- api/libs/services/gen_answer.py
+++ api/libs/services/gen_answer.py
@@ -31,7 +31,7 @@ async def chat_with_job_info(info, messages, model, host, api_key, params):
         start = time.perf_counter()
         max_tokens = params.get("max_tokens", 1000)
         new_params = dict(**params)
-        new_params["max_tokens"] = max_tokens + 500
+        new_params["max_tokens"] = max_tokens + 300
         response = await chat(messages, model, host, api_key, new_params)
         end = time.perf_counter()
 

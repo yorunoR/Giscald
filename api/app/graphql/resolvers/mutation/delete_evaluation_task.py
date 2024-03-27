7d40658d56074032cc28d0de8046329f7194ceed
Yoshioka <sss.yoshioka@gmail.com>
2024-03-27 15:08:02 +0000

    🎸 feat: add delete_generation_task and delete_evaluation_task mutations

---
+++ api/app/graphql/resolvers/mutation/delete_evaluation_task.py
@@ -0,0 +1,12 @@
+from strawberry import ID
+from strawberry.types import Info
+
+from libs.models import EvaluationTask
+
+
+async def resolve(info: Info, id: ID):
+    user = info.context.user
+    evaluation_task = await EvaluationTask.objects.aget(id=id, user=user)
+    result = await EvaluationTask.objects.filter(id=id, user=user).adelete()
+
+    return evaluation_task

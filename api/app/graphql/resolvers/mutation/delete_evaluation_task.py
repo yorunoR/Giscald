58222588c7eb7f1be0382bc012f5f3717ad384be
Yoshioka <sss.yoshioka@gmail.com>
2024-03-27 23:17:02 +0000

    🐛 fix: ruff check

--- api/app/graphql/resolvers/mutation/delete_evaluation_task.py
+++ api/app/graphql/resolvers/mutation/delete_evaluation_task.py
@@ -7,6 +7,6 @@ from libs.models import EvaluationTask
 async def resolve(info: Info, id: ID):
     user = info.context.user
     evaluation_task = await EvaluationTask.objects.aget(id=id, user=user)
-    result = await EvaluationTask.objects.filter(id=id, user=user).adelete()
+    await EvaluationTask.objects.filter(id=id, user=user).adelete()
 
     return evaluation_task
--- api/app/graphql/resolvers/mutation/delete_generation_task.py
+++ api/app/graphql/resolvers/mutation/delete_generation_task.py
@@ -7,6 +7,6 @@ from libs.models import GenerationTask
 async def resolve(info: Info, id: ID):
     user = info.context.user
     generation_task = await GenerationTask.objects.aget(id=id, user=user)
-    result = await GenerationTask.objects.filter(id=id, user=user).adelete()
+    await GenerationTask.objects.filter(id=id, user=user).adelete()
 
     return generation_task

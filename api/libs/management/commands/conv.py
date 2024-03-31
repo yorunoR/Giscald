be7125eed4aa60b1b2f2b5041ea01bf9bbd45d59
Yoshioka <sss.yoshioka@gmail.com>
2024-03-31 23:51:19 +0900

    🎸 feat: one time convert command for migration of relation

---
+++ api/libs/management/commands/conv.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from libs.models import Answer, Question
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "conv database."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--mode", type=str, help="Mode")
+
+    def handle(self, *args, **options):
+        self.main()
+
+    def main(self):
+        run()
+
+
+def run():
+    for answer in Answer.objects.all():
+        content = answer.messages[0]["content"]
+        for question in Question.objects.all():
+            if content == question.turns[0]:
+                print(content)
+                answer.question = question
+                answer.save()

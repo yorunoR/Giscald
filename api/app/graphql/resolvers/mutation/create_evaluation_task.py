556afa891f60332a78ad924429f02feb30f0c7ed
Yoshioka <sss.yoshioka@gmail.com>
2024-05-07 01:24:57 +0900

    🎸 feat: Bench can be selected

--- api/app/graphql/resolvers/mutation/create_evaluation_task.py
+++ api/app/graphql/resolvers/mutation/create_evaluation_task.py
@@ -46,14 +46,15 @@ async def resolve(info: Info, generation_task_id: ID, eval_name: str, model: str
         jobs = []
         async for answer in generation_task.answers.select_related("question").order_by("id").all():
             question = answer.messages[0]["content"]
-            content = template.format(question=question, answer=answer.text)
+            correct_answer = answer.question.correct_answers[0] if answer.question.correct_answers else None
+            eval_aspect = answer.question.eval_aspects[0] if answer.question.eval_aspects else None
+            content = template.format(question=question, answer=answer.text, correct_answer=correct_answer, eval_aspect=eval_aspect)
             messages = [
                 {"role": "system", "content": "評価の点数は必ず[[数字]]の形式で示す。説明は簡潔にする。"},
                 {"role": "user", "content": content},
             ]
-            correct_answers = answer.question.correct_answers
-            if correct_answers:
-                messages.append({"role": "user", "content": f"正しい答えは次のようになります。{correct_answers[0]}"})
+            # if correct_answers:
+            #     messages.append({"role": "user", "content": f"正しい答えは次のようになります。{correct_answers[0]}"})
             params = {"temperature": 0, "max_tokens": 1500}
             jobs.append(chat_with_job_info(answer, messages, model, host=None, api_key=api_key, params=params))
             if len(jobs) == worker_count:
@@ -63,10 +64,13 @@ async def resolve(info: Info, generation_task_id: ID, eval_name: str, model: str
                     try:
                         if isinstance(result, asyncio.exceptions.CancelledError):
                             raise Exception("Timeout")
-                        match = re.search(r"\[\[(.+)\]\]", result["response"]["answer"])
+                        answer = result["response"]["answer"]
+                        match = re.search(r"\[\[(.+)\]\]", answer)
                         point = 0
                         if match is not None:
                             point = extract_and_convert_to_int(match.group(1))
+                        if point == 0 and answer.isdigit():
+                            point = int(answer)
                         await Rate.objects.acreate(
                             user=user,
                             evaluation_task=evaluation_task,

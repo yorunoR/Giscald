9710994a2980541b2fc1e1aa48d052fa7c4aa357
Yoshioka <sss.yoshioka@gmail.com>
2024-07-26 03:39:11 +0900

    🎸 feat: add presets for SGLang

--- web/src/data/presets/index.ts
+++ web/src/data/presets/index.ts
@@ -1,3 +1,4 @@
 export { tgiSet } from './tgiSet'
 export { vllmSet } from './vllmSet'
+export { sglSet } from './sglSet'
 export { otherSet } from './otherSet'
---
+++ web/src/data/presets/sglSet.ts
@@ -0,0 +1,12 @@
+export const sglSet = {
+  default: {
+    strategy: 'none',
+    params: {
+      max_tokens: 1000,
+      temperature: 0.1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      top_p: 0.99
+    }
+  }
+}
